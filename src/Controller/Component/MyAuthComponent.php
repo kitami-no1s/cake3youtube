b78272dde3319a7e706e545771fb0a33aaba449c
Michiru Sato <sato.michiru@no1s.biz>
2018-06-18 16:58:52 +0900

    My Auth

--- src/Controller/Component/MyAuthComponent.php
+++ src/Controller/Component/MyAuthComponent.php
@@ -1,57 +1,58 @@
-<?php 
+<?php
 namespace App\Controller\Component;
 
 use Cake\Controller\Component;
+use Cake\Controller\Component\AuthComponent;
 use Cake\ORM\TableRegistry;
 use Cake\Event\Event;
-use Cake\Controller\Component\AuthComponent;
 
-//AuthComponentを継承した自作のコンポーネント
 class MyAuthComponent extends AuthComponent
 {
-	public $name="MyAuth";
-	
+	public $name = "MyAuth";
+
 	public function initialize(array $config)
 	{
 		parent::initialize($config);
-		$defaults=[
-				"authorize"=>"Controller",
-				"authenticate"=>[
-					"Form"=>[
-						"userModel"=>"Users",
-						"fields"=>[
-							"username"=>"email",
-							"password"=>"password",
+		$defaults = [
+				"authorize" => "Controller",
+				"authenticate" => [
+						"Form" =>[
+								"userModel" => "Users",
+								"fields" =>[
+										"username" => "email",
+										"password" => "password",
+								],
 						],
-					],
 				],
-				"loginRedirect"=>[
-						"controller"=>"Playlists",
-						"action"=>"index",
-						"prefix"=>"admin"
+				"loginRedirect" => [
+						"controller" => "Playlists",
+						"action" => "index",
+						"prefix" => "admin"
 				],
-				"logoutRedirect"=>[
-						"controller"=>"Playlists",
-						"action"=>"index",
-						"prefix"=>false
+				"logoutRedirect" => [
+						"controller" => "Playlists",
+						"action" => "index",
+						"prefix" => false
 				],
-				"loginAction"=>[
-						"controller"=>"Users",
-						"action"=>"login",
-						"prefix"=>false
+				"loginAction" =>[
+						"controller" => "Users",
+						"action" => "login",
+						"prefix" => false
 				],
-				"authError"=>"ログインする必要があります",
-				"flash"=>[
-						"key"=>"auth",
-						"element"=>"error",
-						"duplicate"=>false
+				"authError" => "ログインする必要があります",
+				"flash" =>[
+						"key" => "auth",
+						"element" => "error",
+						"duplicate" => false
 				],
 		];
 		foreach ($config as $key => $value){
-			if($value !==null){
+			if($value !== null){
 				unset($defaults[$key]);
 			}
 		}
 		$this->config($defaults);
 	}
-}?>
+}
+
+?>
