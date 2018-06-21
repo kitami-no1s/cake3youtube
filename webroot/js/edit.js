ebcb3d7435afa8acab6ee827a2b242a433750771
Michiru Sato <sato.michiru@no1s.biz>
2018-06-21 15:06:57 +0900

    いろいろ

---
+++ webroot/js/edit.js
@@ -0,0 +1,75 @@
+$(function(){
+	$("#form").submit(adminVideoDeleteRequest);
+});
+function
+
+
+
+
+$(function(){
+    $("#form").submit(function(){
+        //選択されたチェックボックスの値を配列に保存
+        var v_code=[];
+        $("[name='v_code[]']:checked").each(function(){
+            v_code.push(this.value);
+        });
+        adminDeleteFormInit();
+        var playlist_id = $('.index').attr('id');
+
+        $.ajax({
+            type: "POST",
+            url: "/cake3youtube/admin/mylists/deleteajax",
+            data: {
+                "v_code":v_code,
+                "playlist_id":playlist_id
+            },
+    		dataType:"json",
+    		success:adminDeleteVideoSuccess,
+    		error:adminDeleteVideoError
+        });
+    });
+});
+    function adminDeleteFormInit(){
+    	$('#message').remove();
+    	$('.help-block').remove();
+    	$('.form-group').removeClass('has-error');
+    }
+    function adminDeleteVideoSuccess(result){
+    	if(result['status'] == 'success'){
+    		showSuccessMessage("削除しました")
+    	}else{
+    		showErrorMessage("削除に失敗しました");
+    		showValidationMessage(result['errors']);
+    	}
+    }
+
+    function adminDeleteVideoError(result){
+    	showErrorMessage("エラーが発生しました");
+    }
+
+    function showSuccessMessage(message){
+    	var tag = '<div id="message" class="alert alert-success">';
+    	tag += message;
+    	tag += '</div>';
+    	$('.main').prepend(tag);
+    }
+
+    function showErrorMessage(message){
+    	var tag = '<div id="message" class="alert alert-danger">';
+    	tag += message;
+    	tag += '</div>';
+    	$('.main').prepend(tag);
+    }
+
+    function showValidationMessage(errors){
+    	for(key in errors){
+    		var obj=$("[name='" + key +"']");
+    		obj.parent().addClass('has-error');
+    		var field = errors[key];
+    		for(var value in field){
+    			var tag ='<div class="help-block">' + field[value] + '</div>';
+    			obj.after(tag);
+    		}
+    	}
+    }
+    
